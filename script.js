ac577559b812e61b63abb5999822cd10e9123acf
phischa <ph.git@mailbox.org>
2024-01-29 23:12:41 +0100

    third commit

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-let
+let posts = [
+    {
+        'author': 'Tagesschau',
+        'image': 'conent/img1.jpg',
+        
+    }
+
+]
+
+
+let authors = ['tagesschau', 'tagesschau', 'tagesschau', 'tagesschau'];
+let images = ['content/img1.jpg', 'content/img2.jpg', 'content/img3.jpg', 'content/img4.jpg']
+let headline = []
+
+
 
 function render() {
 
