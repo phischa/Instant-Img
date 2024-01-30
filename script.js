08c47d87c69a6ed6978983780910f8f15e342947
phischa <ph.git@mailbox.org>
2024-01-30 10:55:15 +0100

    first script

--- script.js
+++ script.js
@@ -4,16 +4,27 @@ let posts = [
         'image': 'conent/img1.jpg',
         
     }
+    {
+        'author': 'Tagesschau',
+        'image': 'conent/img1.jpg',
 
+    }
+    {
+        'author': 'Tagesschau',
+        'image': 'conent/img1.jpg',
+
+    }
 ]
 
 
-let authors = ['tagesschau', 'tagesschau', 'tagesschau', 'tagesschau'];
+let authors = ['tagesschau', 'tagesschau', 'tagesschau'];
 let images = ['content/img1.jpg', 'content/img2.jpg', 'content/img3.jpg', 'content/img4.jpg']
 let headline = []
 
 
 
 function render() {
+    let content = document.getElementById('content');
+    content.innerHTML = '';
 
 }
