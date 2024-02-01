716113da70c255bdf461268f8959cf04e447dee4
phischa <ph.git@mailbox.org>
2024-02-02 00:04:56 +0100

    mobile adjustments

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let posts = [
         "img": "content/pic4.jpg",
         "likes": 82,
         "liked": false,
-        "description": "Wohnbauserie 70 (WBS 70)",
+        "description": "BlaBlaBla",
         "comments": [],
         "commentAuthor": [],
     },
@@ -22,7 +22,7 @@ let posts = [
     {
         "author": "natural_nature",
         "authorImg": "content/profil1.jpg",
-        "img": "content/fox1",
+        "img": "content/fox1.jpg",
         "likes": 165,
         "liked": false,
         "description": "BlaBla",
@@ -32,72 +32,6 @@ let posts = [
 ]
 
 load();
-
-function render() {
-    let content = document.getElementById('content');
-    content.innerHTML += '';
-
-    for (let i = 0; i < posts.length; i++) {
-        const post = posts[i];
-
-        content.innerHTML += renderHTML(post, i);
-        renderComments();
-        save();
-    }
-}
-
-function renderHTML(post, i) {
-    return /* HTML */ `
-    <div class="card">
-        <div class="author-area">
-            <img class="author-img" src="${post["authorImg"]}" alt="Profil Picture">
-            <span class="author"><b>${post["author"]}</b></span>
-        </div>
-        <img class="post-img" src="${post["img"]}" alt="Image">
-        <div>
-            <div class="like-comment">
-                <div class="like-comment-flex">
-                <div>
-                    <img src="img/heart.png" alt="Like">   
-                    <img src="img/comment.png" alt="Comment">
-                    <img src="img/paper-plane.png" alt="Message">
-                </div>
-                <div>
-                    <img class="bookmark" src="img/bookmark.png" alt="Bookmark">
-                </div>
-                </div>
-
-                <p><b>Gefällt ${post["likes"]} mal</b></p>
-                <p>${post["description"]}<p>
-            </div>
-            <div>
-                <div id="commentContent${i}" ></div>
-                <div class="comment-box">
-                    <input class="input-comment" id="input${i}" placeholder="Kommentieren..." type="text">
-                    <button class="button-comment" onclick=""><b>Posten</b></button>
-                </div>
-            </div>
-        </div>
-    </div>
-    `;
-}
-
-function renderComments(i) {
-    let commentContent = document.getElementById(`commentContent${i}`);
-
-        for (let j = 0; j < posts['comments'].length; j++) {
-            const comment = posts['comments'][j];
-            commentContent.innerHTML += /* HTML */ `<div>${comment}</div>`;
-        }
-}
-
-function addComment(index) {
-    let input = document.getElementById(`input${index}`);
-    posts[index]["comments"].push(input.value);
-  
-    render();
-    save();
-  }
 
 /* SAVE AND LOAD */
 function load() {
@@ -110,4 +44,86 @@ function load() {
 function save() {
     let postsAsText = JSON.stringify(posts);
     localStorage.setItem("posts", postsAsText);
+  }
+
+/* render */
+function render() {
+    let content = document.getElementById('content');
+    content.innerHTML += '';
+
+    for (let i = 0; i < posts.length; i++) {
+        const post = posts[i];
+
+        content.innerHTML += renderHTML(post, i);
+        renderComments(i);
+        save();
+    }
+}
+
+function renderHTML(post, i) {
+    return /* HTML */ `
+    <div class="card">
+        <div class="author-area">
+            <img class="author-img" src="${post['authorImg']}" alt="Profil Picture">
+            <span class="author"><b>${post['author']}</b></span>
+        </div>
+        <img class="post-img" src="${post['img']}" alt="Image">
+        <div>
+            <div class="like-comment">
+                <div class="like-comment-flex">
+                <div>
+                    <img src="${heartLiked(i)}" onclick="addLikec(${i})" alt="Like">   
+                    <img src="img/comment.png" alt="Comment">
+                    <img src="img/paper-plane.png" alt="Message">
+                </div>
+                <div>
+                    <img class="bookmark" src="img/bookmark.png" alt="Bookmark">
+                </div>
+                </div>
+
+                <p><b>Gefällt ${post['likes']} mal</b></p>
+                <p>${post['description']}<p>
+            </div>
+            <div>
+                <div class="comment" id="commentContent${i}"></div>
+                <div class="input-box">
+                    <input class="input-comment" id="inputComment${i}" placeholder="Kommentieren..." type="text">
+                    <button class="button-comment" onclick="addComment(${i})"><b>Posten</b></button>
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
+}
+
+/* comments */
+function renderComments(i) {
+    let commentContent = document.getElementById(`commentContent${i}`);
+
+        for (let j = 0; j < posts[i]["comments"].length; j++) {
+            const comment = posts[i]["comments"][j];
+            commentContent.innerHTML += /* HTML */ `
+            <div>${comment}</div>`;
+        }
+}
+
+function addComment(i) {
+    let input = document.getElementById(`inputComment${i}`);
+    posts[i]['comments'].push(input.value);
+    input.value = "";
+    render();
+    save();
+  }
+
+  /* likes */
+  function addLike(i) {
+
+  }
+
+  function heartLiked(i) {
+    if (posts[i]["liked"]) {
+      return "/img/heart2.png";
+    } else {
+      return "/img/heart1.png";
+    }
   }
